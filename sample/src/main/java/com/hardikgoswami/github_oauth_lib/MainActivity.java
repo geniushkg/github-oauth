0f09af1a45d1eeaadbebccac51a405267865186a
geniushkg <geniushkg@gmail.com>
2016-11-05 12:36:51 +0530

    chore : bintray upload change artifact id

--- sample/src/main/java/com/hardikgoswami/github_oauth_lib/MainActivity.java
+++ sample/src/main/java/com/hardikgoswami/github_oauth_lib/MainActivity.java
@@ -1,13 +1,10 @@
 package com.hardikgoswami.github_oauth_lib;
 
 import android.content.Context;
-import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-
-import com.hardikgoswami.oauthLibGithub.*;
 
 public class MainActivity extends AppCompatActivity {
 
